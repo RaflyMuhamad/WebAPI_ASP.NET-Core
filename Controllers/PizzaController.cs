5cf26030f7b04b392859842f1c5ffab231a77160
RaflyMuhamad <100673383+RaflyMuhamad@users.noreply.github.com>
2022-10-19 08:22:19 +0700

    CRUD method

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -43,13 +43,29 @@ public class PizzaController : ControllerBase
     [HttpPut("{id}")]
     public IActionResult Update(int id, Pizza pizza)
     {
-        // This code will update the pizza and return a result
+        if (id != pizza.Id)
+            return BadRequest();
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
     }
 
     // DELETE action
     [HttpDelete("{id}")]
     public IActionResult Delete(int id)
     {
-        // This code will delete the pizza and return a result
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
     }
 }
